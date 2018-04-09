1b2402908eea4f686c25d8f41314289fe7c69c76
Gonzalo Rocha <grochadc@gmail.com>
2018-04-09 12:20:00 -0500

    Add isLoggedIn middleware to admin dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     router = express.Router(),
     Ad = require("../models/ad"),
-    User = require("../models/user");
+    User = require("../models/user"),
+    middleware = require('../libs/middleware');
 
     router.get("/:user_id/dashboard",function(req,res){
         User.findById(req.params.user_id).populate("ads").exec(function(err,foundUser){
@@ -13,7 +14,7 @@ var express = require('express'),
         });
     });
 
-    router.get("/admin", function(req, res){
+    router.get("/admin", middleware.isLoggedIn, function(req, res){
       User.find({}, function(err, users){
         if(err) console.log(err);
         else res.render("admin-dashboard", {users: users});
